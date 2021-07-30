402ffd91b91ee37c49bbad4fb3533529442a59a0
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-07-30 17:29:05 +1000

    test Readme demo

--- Demo/DEMOREADME.md
+++ Demo/DEMOREADME.md
@@ -1,14 +1,12 @@
 
 
-### d
+### Test Projet
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/d/d)
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
 
 ## Description
 
-d
+The project is a test
 
 
 ## Table of Contents 
@@ -18,30 +16,34 @@ d
 * [Contributing](#contributing)
 * [Tests](#tests)
 * [License](#license)
-* [Questions](#questions)
+* [Questions](#contact)
 
 ##  Installation
-The following necessary dependencies must be installed to run the application properly: d
+The following necessary dependencies must be installed to run the application properly: hello world
 
 ## Usage
-​This application is used for d
+​This application is used for hello
 
 ## License
-This project is license under the mit license.
+This project is license under the mpl-2.0 license.
 
 ## Contributing
-​Contributors: d
+​Contributors: hello
 
 ## Tests
-To run tests, you need to run the following command: d
+To run tests, you need to run the following command: hello
 
 ## License
 
-http://choosealicense.com/licenses/mit/
+[See Details about the mpl-2.0-License: ](http://choosealicense.com/licenses/mpl-2.0/)
 
-A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.
 
 
-## Questions
-If you have any questions about the repo, open an issue or contact d directly undefined.
+## Contact
+
+Please contact me for further Questions:
+
+* [🐈‍⬛ GitHub Profile: ](https://github.com/ZahraMertens)
+* [✉️ Contact via Email: ](mailto:zahra.mertens@gmx.net)
  
--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+const Choices = require('inquirer/lib/objects/choices');
 const fetch = require('node-fetch');
 
 
@@ -35,66 +36,29 @@ function renderLicenseBadge(data) {
     }
   }
   
-  /*![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
-  
-  let licenseString = '';
-  if (licenseType === 'APACHE'){
-    licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
-  }
-  if (licenseType === 'GNU'){
-    licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
-  }
-  if (licenseType === 'IBM'){
-    licenseString = `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
-  }
-  if (licenseType === 'MIT'){
-    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
-  }
-  return licenseString*/
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 /*function renderLicenseLink(data) {
-
-  const licenseKind = data.license;
-  console.log(licenseKind)
-
-  fetch(`https://api.github.com/licenses/${licenseKind}`)
-   .then(res => res.json())
-   .then(data => {
-            console.log(data);
-            const licUrl = data.html_url
-            console.log(licUrl)
-            return licUrl
-        })
-        .catch((err) => {
-            console.log(err);
-  })
-    
 }*/
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 /*function renderLicenseSection(data) {
   
- // const licenseLink = data.html_url;
- // const licenseDes = data.licDescription;
- // return licenseLink, licenseDes
 }*/
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data, licUrl, licDes) {
 
-  var licenseBadge = renderLicenseBadge(data);
+  var licenseBadge = renderLicenseBadge(data); 
 
   return `
 
 ### ${data.title}
 
 ${licenseBadge}
-
-[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/${data.username}/${data.title})
 
 ## Description
 
@@ -108,7 +72,7 @@ ${data.description}
 * [Contributing](#contributing)
 * [Tests](#tests)
 * [License](#license)
-* [Questions](#questions)
+* [Questions](#contact)
 
 ##  Installation
 The following necessary dependencies must be installed to run the application properly: ${data.installation}
@@ -127,13 +91,17 @@ To run tests, you need to run the following command: ${data.test}
 
 ## License
 
-${licUrl}
+[See Details about the ${data.license}-License: ](${licUrl})
 
 ${licDes}
 
 
-## Questions
-If you have any questions about the repo, open an issue or contact ${data.username} directly ${data.erfmail}.
+## Contact
+
+Please contact me for further Questions:
+
+* [🐈‍⬛ GitHub Profile: ](https://github.com/${data.username})
+* [✉️ Contact via Email: ](mailto:${data.email})
  
 `;
 }
